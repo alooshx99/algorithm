f80d94b393d1185c10ed16e97574dbbcec421bb2
Ali Al Khattawi <124931846+alooshx99@users.noreply.github.com>
2024-11-27 17:43:20 +0330

    Add files via upload

---
+++ 12-A-KnapsackProblem.py
@@ -0,0 +1,38 @@
+"""
+Solve the 0/1 knapsack problem using dynamic programming.
+with sorting
+Example: Weights: [2, 3, 4, 5], Values: [3, 4, 5, 6], Capacity: 5 → Output: 6
+"""
+
+def knapsack(weights, values, capacity):
+    arr =[]
+    subArr = []
+    for i in range(len(weights)):
+        subArr.append(values[i]/weights[i])
+        subArr.append(values[i])
+        subArr.append(weights[i])
+        arr.append(subArr)
+        subArr = []
+
+    arr.sort()
+
+    knapsackValue = 0
+
+    while capacity > 0:
+        for item in arr:
+            if capacity - item[2] < 0:
+                continue
+            else:
+                capacity -= item[2]
+                knapsackValue += item[1]
+
+
+
+
+    return knapsackValue
+
+
+
+
+
+print(knapsack([2, 3, 4, 5],[3, 4, 5, 6],5))
